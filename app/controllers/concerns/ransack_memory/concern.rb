0c55fe90c31f0f5688df0765ae17202447621ae4
David Bull <david.bull@bt.com>
2023-01-24 16:26:50 +0000

    Allow custom ransack param name to be passed to save_and_load_filters

--- app/controllers/concerns/ransack_memory/concern.rb
+++ app/controllers/concerns/ransack_memory/concern.rb
@@ -2,7 +2,7 @@ module RansackMemory
   module Concern
     extend ActiveSupport::Concern
 
-    def save_and_load_filters
+    def save_and_load_filters(ransack_param_name = ::RansackMemory::Core.config[:param])
       user_set_key_identifier = respond_to?(:set_session_key_identifier) ? send(:set_session_key_identifier) : nil
 
       session_key_identifier = ::RansackMemory::Core.config[:session_key_format]
@@ -14,7 +14,7 @@ module RansackMemory
       session_key_base = "ranmemory_#{session_key_base}" unless session_key_base.starts_with?('ranmemory')
 
       # permit search params
-      params[::RansackMemory::Core.config[:param]].permit! if params[::RansackMemory::Core.config[:param]].present? && params[::RansackMemory::Core.config[:param]].respond_to?(:permit)
+      params[ransack_param_name].permit! if params[ransack_param_name].present? && params[ransack_param_name].respond_to?(:permit)
 
       # cancel filter if button pressed
       if params[:cancel_filter] == "true"
@@ -24,7 +24,7 @@ module RansackMemory
       end
 
       # search term saving
-      session["#{session_key_base}"] = params[::RansackMemory::Core.config[:param]].to_h if params[::RansackMemory::Core.config[:param]].present?
+      session["#{session_key_base}"] = params[ransack_param_name].to_h if params[ransack_param_name].present?
 
       # page number saving
       session["#{session_key_base}_page"] = params[:page] if params[:page].present?
@@ -33,7 +33,7 @@ module RansackMemory
       session["#{session_key_base}_per_page"] = params[:per_page] if params[:per_page].present?
 
       # search term load
-      params[::RansackMemory::Core.config[:param]] = session["#{session_key_base}"] if session["#{session_key_base}"].present?
+      params[ransack_param_name] = session["#{session_key_base}"] if session["#{session_key_base}"].present?
 
       # page number load
       params[:page] = session["#{session_key_base}_page"].presence
@@ -42,12 +42,12 @@ module RansackMemory
       params[:per_page] = session["#{session_key_base}_per_page"].presence
 
       # set page number to 1 if filter has changed
-      if (params[::RansackMemory::Core.config[:param]].present? && session[:last_q_params] != params[::RansackMemory::Core.config[:param]]) || (params[:cancel_filter].present? && session["#{session_key_base}_page"] != params[:page])
+      if (params[ransack_param_name].present? && session[:last_q_params] != params[ransack_param_name]) || (params[:cancel_filter].present? && session["#{session_key_base}_page"] != params[:page])
         params[:page] = nil
         session["#{session_key_base}_page"] = nil
       end
 
-      session[:last_q_params] = params[::RansackMemory::Core.config[:param]]&.to_unsafe_h
+      session[:last_q_params] = params[ransack_param_name]&.to_unsafe_h
 
       # session[:last_page] = params[:page]
     end
